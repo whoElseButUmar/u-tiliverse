3c13571b5939b437698ed0f0645ec766741614d8
Omar Aderkaoui <umaraderkaoui@gmail.com>
2024-07-21 09:10:53 +0100

    refactor initalial data to zero

--- src/apps/MissionCodec/hooks/useMissionForm.ts
+++ src/apps/MissionCodec/hooks/useMissionForm.ts
@@ -11,14 +11,12 @@ interface MissionFormData {
 
 export const useMissionForm = () => {
   const [formData, setFormData] = useState<MissionFormData>({
-    missionNumber: "8",
-    missionTitle: "Restructure the ColorConfigBlock",
-    reason:
-      "Refactor the ColorConfigBlock component to have the same structure so that it is easy to use and maintain + refactor the filterOptions.ts to be more universal and not strictly to toggle buttons type",
-    startTime: "15:00 PM",
-    endTime: "16:06 PM",
-    commitLink:
-      "https://github.com/mujud/pathways/commit/bdc66c70caefa038252b63861720beae4c13a5d4",
+    missionNumber: "",
+    missionTitle: "",
+    reason: "",
+    startTime: "",
+    endTime: "",
+    commitLink: "",
   });
 
   const handleInputChange = (
