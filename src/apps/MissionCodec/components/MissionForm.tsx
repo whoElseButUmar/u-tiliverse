7e25feaba0aa3fc4e6cdad6d97ea6a74fd75580a
Omar Aderkaoui <umaraderkaoui@gmail.com>
2024-07-21 09:26:26 +0100

    refactor mission form

--- src/apps/MissionCodec/components/MissionForm.tsx
+++ src/apps/MissionCodec/components/MissionForm.tsx
@@ -18,26 +18,26 @@ export const MissionForm: React.FC<MissionFormProps> = ({
   formData,
   handleInputChange,
 }) => (
-  <div className="w-full max-w-md bg-slate-800 rounded-lg shadow-lg p-6 mb-8">
-    <h2 className="text-2xl font-bold mb-4 text-center text-green-500">
-      MGS Dev Mission Card Maker
+  <div className="w-full bg-slate-800 rounded-lg shadow-lg p-6 flex flex-col gap-4 max-w-md">
+    <h2 className="text-2xl font-bold text-center text-green-500">
+      Mission Codec Card Maker
     </h2>
     <input
-      className="w-full p-2 mb-4 border rounded bg-slate-700 text-white"
+      className="w-full p-2  border rounded bg-slate-700 text-white"
       placeholder="Mission Number"
       name="missionNumber"
       value={formData.missionNumber}
       onChange={handleInputChange}
     />
     <input
-      className="w-full p-2 mb-4 border rounded bg-slate-700 text-white"
+      className="w-full p-2  border rounded bg-slate-700 text-white"
       placeholder="Mission Title"
       name="missionTitle"
       value={formData.missionTitle}
       onChange={handleInputChange}
     />
     <textarea
-      className="w-full p-2 mb-4 border rounded bg-slate-700 text-white"
+      className="w-full p-2  border rounded bg-slate-700 text-white"
       placeholder="Reason"
       name="reason"
       value={formData.reason}
@@ -45,21 +45,23 @@ export const MissionForm: React.FC<MissionFormProps> = ({
       rows={3}
     />
     <input
-      className="w-full p-2 mb-4 border rounded bg-slate-700 text-white"
+      className="w-full p-2  border rounded bg-slate-700 text-white"
       placeholder="Start Time"
       name="startTime"
+      type="time"
       value={formData.startTime}
       onChange={handleInputChange}
     />
     <input
-      className="w-full p-2 mb-4 border rounded bg-slate-700 text-white"
+      className="w-full p-2  border rounded bg-slate-700 text-white"
       placeholder="End Time"
       name="endTime"
+      type="time"
       value={formData.endTime}
       onChange={handleInputChange}
     />
     <input
-      className="w-full p-2 mb-4 border rounded bg-slate-700 text-white"
+      className="w-full p-2  border rounded bg-slate-700 text-white"
       placeholder="Commit Link"
       name="commitLink"
       value={formData.commitLink}
