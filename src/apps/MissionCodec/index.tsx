2715713ae56b4ab141af383e57828b939e3b0a4b
Omar Aderkaoui <umaraderkaoui@gmail.com>
2024-07-21 09:03:45 +0100

    back button cmp

--- src/apps/MissionCodec/index.tsx
+++ src/apps/MissionCodec/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useMissionForm } from "./hooks/useMissionForm";
 import { MissionForm } from "./components/MissionForm";
 import { MissionCard } from "./components/MissionCard";
-import { FaArrowLeft } from "@react-icons/all-files/fa/FaArrowLeft";
 import { AppId } from "../../types/app";
+import { BackButton } from "../utils/BackButton";
 
 interface MissionCodecProps {
   onBack: (returnTo: AppId | null) => void;
@@ -12,21 +12,9 @@ interface MissionCodecProps {
 const MissionCodec: React.FC<MissionCodecProps> = ({ onBack }) => {
   const { formData, handleInputChange } = useMissionForm();
 
-  const handleBack = () => {
-    onBack(null); // null indicates return to the landing page
-  };
-
   return (
     <div className="flex flex-col items-center p-4 bg-slate-900 min-h-screen">
-      <div className="w-full max-w-md mb-4">
-        <button
-          onClick={handleBack}
-          className="flex items-center text-green-500 hover:text-green-400 transition-colors duration-200"
-        >
-          <FaArrowLeft className="mr-2" />
-          Back to Landing Page
-        </button>
-      </div>
+      <BackButton onBack={onBack} />
       <MissionForm formData={formData} handleInputChange={handleInputChange} />
       <MissionCard {...formData} />
     </div>
---
+++ src/apps/utils/BackButton.tsx
@@ -0,0 +1,23 @@
+import { FaArrowLeft } from "@react-icons/all-files/fa/FaArrowLeft";
+import { AppId } from "../../types/app";
+
+export const BackButton = ({
+  onBack,
+}: {
+  onBack: (returnTo: AppId | null) => void;
+}) => {
+  const handleBack = () => {
+    onBack(null); // null indicates return to the landing page
+  };
+  return (
+    <div className="w-full max-w-md mb-4">
+      <button
+        onClick={handleBack}
+        className="flex items-center text-green-500 hover:text-green-400 transition-colors duration-200"
+      >
+        <FaArrowLeft className="mr-2" />
+        Back to Landing Page
+      </button>
+    </div>
+  );
+};
